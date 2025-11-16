375fda6877c55d843b0c0bd2c7496cdb9efd9d42
Xenobii <steliosalvanos@gmail.com>
2025-11-16 12:43:38 +0200

    train page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,52 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route('/train_model', methods=['GET'])
+def train_model():
+    mode = request.args.get("category")
+    MODE_INFO = {
+        "segmentation": {
+            "models": [
+                ("UNetlite", "2D Images"),
+                ("UNet", "2D Images")
+            ]
+        },
+        "detection": {
+            "models": [
+                ("UNetlite", "2D Images"),
+                ("UNet", "2D Images")
+            ]
+        },
+        "classification": {
+            "models": [
+                ("UNetlite", "2D Images"),
+                ("UNet", "2D Images")
+            ]
+        },
+    }
+    if mode is None:
+        mode = "segmentation"
+        
+    data = MODE_INFO[mode]
+    
+    return render_template(
+        "train_model.html",
+        mode            = mode,
+        model_list      = data["models"],
+        collection_list = []
+    )
+
+
+@app.route('/collections')
+def collections():
+    return render_template("collections.html")
+
+
+@app.route('/models')
+def models():
+    return render_template("models.html")
 
 
 if __name__ == "__main__":
